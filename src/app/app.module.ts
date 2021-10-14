03874508f94c153d7a874c1ae0ea99ebaf1fb14f
satya <genti.satyaprakash@gmail.com>
2021-10-14 11:47:15 +0530

    added property list using http get call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -16,7 +16,8 @@ import { PropertyListComponent } from './property-list/property-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/property-list/property-list.component.ts
+++ src/app/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-property-list',
@@ -7,60 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  Properties:Array<any>=[
-  {
-    "Id":1,
-    "Name":"Birla",
-    "Type":"House",
-    "Price":150000
-  },
-  {
-    "Id":2,
-    "Name":"Gachibowli",
-    "Type":"Flat",
-    "Price":670000
-  },
-  {
-    "Id":3,
-    "Name":"Ramanthapur",
-    "Type":"Single BHK",
-    "Price":250000
-  },
-  {
-    "Id":4,
-    "Name":"AshokNagar",
-    "Type":"House",
-    "Price":150000
-  },
-  {
-    "Id":5,
-    "Name":"Birla",
-    "Type":"Flat",
-    "Price":190000
-  },
-  {
-    "Id":6,
-    "Name":"Saroornagar",
-    "Type":"Flat",
-    "Price":150000
-  },
-  {
-    "Id":7,
-    "Name":"Saroornagar",
-    "Type":"2 BHK",
-    "Price":1150000
-  },
-  {
-    "Id":8,
-    "Name":"Saroornagar",
-    "Type":"Individual",
-    "Price":1250000
-  }
+  Properties:any;
+  constructor(private http:HttpClient) { }
 
-]
-  constructor() { }
+  ngOnInit():void {
 
-  ngOnInit() {
+    this.http.get('data/properties.json').subscribe
+    (
+    data=>{
+      this.Properties=data;
+      console.log(data);
+    }
+
+    );
   }
 
 }
